8662392ac5f40af8b39ec3f85b152f339a2563a5
AjayPrajapathi <ajayprajapathi4@gmail.com>
2024-12-06 12:25:47 +0530

    client is nearly created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext'
 
-import App from "./App.jsx";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons"; // Solid icons
-import { fab } from "@fortawesome/free-brands-svg-icons"; // Brand icons
-library.add(fas, fab);
-
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>,
+)
