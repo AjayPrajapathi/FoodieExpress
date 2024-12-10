9f8a17789d8dac7b7d23cbc7688a673375dee3af
AjayPrajapathi <ajayprajapathi4@gmail.com>
2024-12-10 14:39:04 +0530

    main.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,22 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 5173 },
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  base: '/', // Update this if you are deploying to a subpath
-});
+  server: { port: 3000},
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   },
+//   server: {
+//     port: 3000,
+//   },
+//   base: '/', // Update this if you are deploying to a subpath
+// });
